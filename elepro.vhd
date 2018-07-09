ff4a592deeefc508fc74aaf2c8c6acf87f74e478
xxchris1996 <xxchris1996@outlook.com>
2018-07-09 15:32:46 +0800

    modified Please enter the commit message for your changes. Lines starting

--- elepro.vhd
+++ elepro.vhd
@@ -49,7 +49,9 @@ else
                          mylift<=doorwait4;                                                           
        when doorwait4 => mylift<=doorclose;                                                           
        when doorclose =>
-            doorlight<='0';                 --电梯门关                                                            
+            doorlight<='0';                 --电梯门关
+            clearup<='0';
+            cleardn<='0';                                                            
             if udsig='0' then              --电梯处于上升模式                                               
               if position=6 then                                                         
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000" then 
@@ -63,14 +65,14 @@ else
                      udsig<='0';mylift<=doorclose;                                                      
                  elsif stoplight(6)='1' or fdnlight(6)='1'   then  	
                      udsig<='0';mylift<=up;
-                 elsif stoplight(5)='1' or fdnlight(5)='1'   then  	
+                 elsif stoplight(5)='1' or fuplight(5)='1'   then  	
                      udsig<='0';mylift<=stop;    
                  else udsig<='1';mylift<=down;                                            
                  end if;
              elsif position=4 then                                                  
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
                      udsig<='0';mylift<=doorclose;                                                      
-                 elsif stoplight(4)='1' or fdnlight(4)='1'   then  	
+                 elsif stoplight(4)='1' or fuplight(4)='1'   then  	
                      udsig<='0';mylift<=stop; 
                  elsif stoplight>"001000" or fdnlight>"001000" or fuplight>"001000"   then  
                      udsig<='0';mylift<=up;
@@ -81,7 +83,7 @@ else
                      udsig<='0';mylift<=doorclose;                                                      
                  elsif stoplight>"000100" or fdnlight>"000100" or fuplight>"000100"   then 
                      udsig<='0';mylift<=up;
-                 elsif stoplight(3)='1' or fdnlight(3)='1'   then  	
+                 elsif stoplight(3)='1' or fuplight(3)='1'   then  	
                      udsig<='0';mylift<=stop; 
                  else udsig<='1';mylift<=down;                                            
                  end if;
@@ -90,7 +92,7 @@ else
                      udsig<='0';mylift<=doorclose;                                                      
                  elsif stoplight>"000010" or fdnlight>"000010" or fuplight>"000010"   then 
                      udsig<='0';mylift<=up;
-                 elsif stoplight(2)='1' or fdnlight(2)='1'   then  	
+                 elsif stoplight(2)='1' or fuplight(2)='1'   then  	
                      udsig<='0';mylift<=stop; 
                  else udsig<='1';mylift<=down;                                            
                  end if;
@@ -105,74 +107,88 @@ else
            elsif udsig='1' then                                                         
               if position=1 then                                                         
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
-                     udsig<='0';mylift<=doorclose;                                                         
+                     udsig<='0';mylift<=doorclose;
+                 elsif stoplight(1)='1' or fuplight(1)='1'   then  	
+                     udsig<='0';mylift<=stop;                                                         
                  else udsig<='0'; mylift<=up;                                                  
                  end if; 
               elsif position=2 then                                                        
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
-                     udsig<='1';mylift<=doorclose;                                                        
+                     udsig<='1';mylift<=doorclose;  
+                 elsif stoplight(2)='1' or fdnlight(2)='1'   then  	
+                     udsig<='1';mylift<=stop;                                                      
                  elsif stoplight<"000010" or fdnlight<"000010" or fuplight<"000010"   then   
-                     udsig<='1';mylift<=down;                                                              
+                     udsig<='1';mylift<=down;                                                             
                  else udsig<='0'; mylift<=up;                                                  
                  end if;
               elsif position=3 then                                                        
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
-                     udsig<='1';mylift<=doorclose;                                                        
+                     udsig<='1';mylift<=doorclose;    
+                 elsif stoplight(3)='1' or fdnlight(3)='1'   then  	
+                     udsig<='1';mylift<=stop;                                                      
                  elsif stoplight<"000100" or fdnlight<"000100" or fuplight<"000100"   then   
                      udsig<='1';mylift<=down;                                                              
                  else udsig<='0'; mylift<=up;                                                  
                  end if;
               elsif position=4 then                                                        
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
-                     udsig<='1';mylift<=doorclose;                                                        
+                     udsig<='1';mylift<=doorclose; 
+                 elsif stoplight(4)='1' or fdnlight(4)='1'   then  	
+                     udsig<='1';mylift<=stop;                                                         
                  elsif stoplight<"001000" or fdnlight<"001000" or fuplight<"001000"   then   
                      udsig<='1';mylift<=down;                                                              
                  else udsig<='0'; mylift<=up;                                                  
                  end if;
               elsif position=5 then                                                        
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
-                     udsig<='1';mylift<=doorclose;                                                        
+                     udsig<='1';mylift<=doorclose;
+                 elsif stoplight(5)='1' or fdnlight(5)='1'   then  	
+                     udsig<='1';mylift<=stop;                                                          
                  elsif stoplight<"010000" or fdnlight<"010000" or fuplight<"010000"   then   
                      udsig<='1';mylift<=down;                                                              
                  else udsig<='0'; mylift<=up;                                                  
                  end if;
               elsif position=6 then                                                       
                  if stoplight="000000" and fuplight="000000" and fdnlight="000000"  then  
-                      udsig<='1';mylift<=doorclose;  
+                      udsig<='1';mylift<=doorclose;
+                 elsif stoplight(6)='1' or fdnlight(6)='1'   then  	
+                     udsig<='1';mylift<=stop;    
                  else udsig<='1'; mylift<=down;
                  end if;
               end if;
              end if;
       when up =>      position<=position+1;pos:=pos+1;                                                                                        
              if  position <6 and( stoplight(pos)='1' or fuplight(pos)='1') then  
-                      mylift<=stop;
+                      mylift<=stop;--if fuplight="000000" and stoplight="000000" and fdnlight<"100000" then  mylift<=down;udsig<='1'; end if;
                elsif position =6 and (stoplight(pos)='1' or fdnlight(pos)='1')   then 
                       mylift<=stop;
-               elsif position <6 and ( fuplight="000000" and fdnlight(pos)='1')  then
+               elsif position <6 and ( fuplight="000000" and fdnlight(pos)='1' and stoplight <= fdnlight)  then
 					  mylift<=stop;
+			   --elsif  position<6 and fuplight="000000" and stoplight="000000" and fdnlight<"100000" then udsig<='1';mylift<=doorclose;
                else mylift<=doorclose;
              end if;
       when down =>                                                       
                  position<=position-1; pos:=pos-1;                                                  
              if  position>1 and (stoplight(pos)='1' or fdnlight(pos)='1') 
-then    mylift<=stop;
+		then    mylift<=stop;--if fdnlight="000000" and stoplight="000000" and fuplight>"000001" then mylift<=up;udsig<='0'; end if;
  elsif position=1 and (stoplight(pos)='1' or fuplight(pos)='1')
          then    mylift<=stop;
-	elsif position>1 and fdnlight="000000" and fuplight(pos)='1' 
-		 then    mylift<=stop;
+	elsif position>1 and fdnlight="000000" and fuplight(pos)='1' and stoplight <= fuplight
+		 then    mylift<=stop; 
+    elsif  position>1 and fdnlight="000000" and stoplight="000000" and fuplight>"000001" then udsig<='0'; mylift<=doorclose;
  else        mylift<=doorclose;
 end if;
          when stop     =>   mylift<=dooropen;                                                                                                                   
          when dooropen =>        
   doorlight<='1';
 if udsig='0' then
-if position<6 and (stoplight(position)='1' or fuplight(position)='1' ) then clearup<='1';
+if position<=6 and (stoplight(position)='1' or fuplight(position)='1' ) then clearup<='1';
 elsif fuplight="000000" and fdnlight(position)='1' then cleardn<='1';
-elsif position=6 and fdnlight(pos)='1' then cleardn<='1';
+elsif position=6 and (fdnlight(pos)='1' or stoplight(pos)='1')  then cleardn<='1';
 else clearup<='0';cleardn<='0';
 end if; 
 elsif udsig='1' then
-if position>1 and (stoplight(position)= '1' or fdnlight(position)='1') then 
+if position>=1 and (stoplight(position)= '1' or fdnlight(position)='1') then 
 cleardn<='1';
 elsif fdnlight="000000" and fuplight(position)='1' then clearup<='1';
 elsif position=1 and fuplight(pos)='1' then clearup<='1';
